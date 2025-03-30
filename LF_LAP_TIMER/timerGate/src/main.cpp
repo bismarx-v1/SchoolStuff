d565d38dcca57cb51beca05fcdffc74cc8a238ba
Natani Simak <matej.simacek.2@gmail.com>
2025-03-30 19:18:49 +0200

    :3

--- LF_LAP_TIMER/timerGate/src/main.cpp
+++ LF_LAP_TIMER/timerGate/src/main.cpp
@@ -1,10 +1,11 @@
-/**
- * DEBUG: Program freezes when going to "running" mode.
- */
 #include <Arduino.h>
 #include "LiquidCrystal_I2C.h"
 
-#define DEBUG 0
+
+#define DEBUG        0
+#define DEBUG_MSPF   1
+#define DISPLAY_TYPE 1  // 0 - soft, 1 - real.
+#define GATE_RETRIGGER_BLOCK_TIME 5000
 
 #define PIN_GATE      2
 #define PIN_BTN_ARM   5
@@ -24,8 +25,7 @@
 #define MS_PER_LOOP 20
 
 #define DISPLAY_TIMER_OFFSET 0
-
-#define DISPLAY_TYPE 1  // 0 - soft, 1 - real.
+#define DISPLAY_TEXT_OFFSET  16
 
 
 /**
@@ -44,7 +44,7 @@
  * @param STUFF The stuff in the if.
  * @brief Sets the last state and does some stuff.
  */
-#define STATE_ON_EXIT(STATE_CUR, STATE_PREV, STUFF)                                                                           \
+#define STATE_ON_EXIT(STATE_CUR, STATE_PREV, STUFF)                                                                            \
   {                                                                                                                            \
     if(STATE_CUR != STATE_PREV) {                                                                                              \
       STUFF                                                                                                                    \
@@ -104,6 +104,7 @@ void setup() {
 #endif
 #if DEBUG == 1
   Serial.print("\x1b[?25l");  // Hide cursor.
+  Serial.print("\x1b[48;5;111m\x1b[38;5;202m");  // Terminal colours.
 #endif
   memset(displayBuffer, 32, DISPLAY_CHARS);  // Clear display.
   deltaTime();
@@ -126,7 +127,7 @@ void loop() {
   uint8_t         timerM;
   bool            displayBufferChanged = 0;
   static uint8_t  tempTick1            = 0xff;
-  static bool            keepFrame            = 0;
+  static bool     keepFrame            = 0;
 
 
 
@@ -140,18 +141,20 @@ void loop() {
   switch(state) {
     case STATE_WAITING:
       STATE_ON_ENTER(
-      state, statePrev, for(uint8_t idx = 0; idx < 7; idx++) { displayBuffer[idx] = "WAITING"[idx]; } displayBufferChanged = 1; keepFrame = 0;)
+      state, statePrev, for(uint8_t idx = 0; idx < 7; idx++) { displayBuffer[idx] = "WAITING"[idx]; } displayBufferChanged = 1;
+      keepFrame = 1;)
 
       if(armChange == 1 && armVal == 0) {  // Arm button.
         state = STATE_ARMED;
       }
 
-      STATE_ON_EXIT(state, statePrev,  keepFrame = 0;)
+      STATE_ON_EXIT(state, statePrev, keepFrame = 0; displayBufferChanged = 1;)
       break;
 
     case STATE_ARMED:
       STATE_ON_ENTER(
-      state, statePrev, for(uint8_t idx = 0; idx < 7; idx++) { displayBuffer[idx] = "ARMED  "[idx]; } displayBufferChanged = 1;  keepFrame = 0;)
+      state, statePrev, for(uint8_t idx = 0; idx < 7; idx++) { displayBuffer[idx] = "ARMED  "[idx]; } displayBufferChanged = 1;
+      keepFrame = 1;)
 
       if(gateChange == 1 && gateVal == 0) {
         state          = STATE_RUNNING;
@@ -162,11 +165,12 @@ void loop() {
         state = STATE_WAITING;
       }
 
-      STATE_ON_EXIT(state, statePrev,  keepFrame = 0;)
+      STATE_ON_EXIT(state, statePrev, keepFrame = 0; displayBufferChanged = 1;)
       break;
     case STATE_RUNNING:
       STATE_ON_ENTER(
-      state, statePrev, for(uint8_t idx = 0; idx < 9; idx++) { displayBuffer[idx + DISPLAY_TIMER_OFFSET] = "000:00:00"[idx];  keepFrame = 0;})
+      state, statePrev,
+      for(uint8_t idx = 0; idx < 9; idx++) { displayBuffer[idx + DISPLAY_TIMER_OFFSET] = "000:00:00"[idx]; } keepFrame = 0;)
 
       if(armChange == 1 && armVal == 0) {
         state = STATE_WAITING;
@@ -181,6 +185,8 @@ void loop() {
       timerM  = (timerTime - timerMs - timerS) / 60000 % 999;
 
 
+      displayBuffer[DISPLAY_TIMER_OFFSET + 3] = ':';
+      displayBuffer[DISPLAY_TIMER_OFFSET + 6] = ':';
 
       for(uint8_t idx = 0; idx < 3; idx++) {
         displayBuffer[idx + DISPLAY_TIMER_OFFSET + 0] = ((timerM / (uint16_t)pow(10, 2 - idx)) % 10) + '0';
@@ -195,7 +201,7 @@ void loop() {
       }
 
       if(gateChange == 1 && gateVal == 0) {
-        if(timerTime > 1000) {
+        if(timerTime > GATE_RETRIGGER_BLOCK_TIME) {
           state = STATE_END;
         }
       }
@@ -206,14 +212,19 @@ void loop() {
       break;
 
     case STATE_END:
-      STATE_ON_ENTER(state, statePrev,  keepFrame = 1;)
+      STATE_ON_ENTER(
+      state, statePrev, keepFrame = 1; for(uint8_t idx = 0; idx < 5; idx++) {
+        displayBuffer[idx + DISPLAY_TEXT_OFFSET] = "ENDED"[idx];
+      } displayBufferChanged = 1;
+
+      )
       if(resetChange == 1 && resetVal == 0) {
         state = STATE_ARMED;
       } else if(stopChange && stopVal == 0) {
         state = STATE_WAITING;
       }
 
-      STATE_ON_EXIT(state, statePrev, keepFrame = 0;)
+      STATE_ON_EXIT(state, statePrev, keepFrame = 0; displayBufferChanged = 1;)
       break;
 
     default:
@@ -223,7 +234,7 @@ void loop() {
 #endif
       break;
   }
-#if DEBUG == 1
+#if DEBUG == 1 || DEBUG_MSPF == 1
   if(tempTick1 >= 32) {
     for(uint8_t idx = 0; idx < 2; idx++) {
       displayBuffer[idx + 28] = ((deltaTimeVar / (uint16_t)pow(10, 1 - idx)) % 10) + '0';
@@ -234,7 +245,6 @@ void loop() {
     tempTick1            = 0;
   }
   tempTick1++;
-
 #endif
 
   if(displayBufferChanged == 1) {
@@ -256,7 +266,7 @@ void loop() {
     Serial.print(stupidDisplayBuffer[1]);
 #endif
     if(keepFrame != 1) {
-        memset(displayBuffer, 32, DISPLAY_CHARS);  // Clear display.
+      memset(displayBuffer, 32, DISPLAY_CHARS);  // Clear display.
     }
   }
 
